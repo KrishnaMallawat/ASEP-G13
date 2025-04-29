8e2cef14fc39cb67918a1fdf10bd092d5fbe1dbd
Aayush Rahate <rahateaayush50@gmail.com>
2025-04-29 18:09:58 +0530

    Add files via upload

---
+++ GanitMitraDungeon/Tiled/FOUNTAIN.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="FOUNTAIN" tilewidth="16" tileheight="16" tilecount="240" columns="15">
+ <image source="../../../Downloads/!$vectoraith_sprite_16x16_fountain_medieval.png" width="240" height="256"/>
+</tileset>
---
+++ GanitMitraDungeon/Tiled/WALKWAYS.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="WALKWAYS" tilewidth="16" tileheight="16" tilecount="192" columns="16">
+ <image source="../../../Downloads/vectoraith_tileset_16x16_terrains_temperate_autotile_rpgmaker.png" width="256" height="192"/>
+</tileset>
