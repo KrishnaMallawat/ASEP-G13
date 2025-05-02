cbc77932e33be5eedf76f77f649a6f39068d8e51
AqibeShaikh09 <foraqibeshaikh@gmail.com>
2025-05-02 20:06:34 +0530

    Python

---
+++ GanitMitraDungeon/assets/playerDown.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1" name="playerDown" tilewidth="12" tileheight="12" tilecount="16" columns="4">
+ <image source="../static/images/ACharDown.png" width="48" height="48"/>
+</tileset>
