82f2758db151d853add49889a97ce2c0251d466a
KrishnaMallawat <krishnamallawat1@gmail.com>
2025-05-09 14:48:09 +0530

    .

--- GanitMitraDungeon/index.js
+++ GanitMitraDungeon/index.js
@@ -2,8 +2,8 @@ const canvas = document.querySelector('canvas')
 
 const c=canvas.getContext('2d')
 
-canvas.width=1024
-canvas.height=576
+canvas.width=1680
+canvas.height=820
 
 c.fillStyle='white'
 c.fillRect(0,0,canvas.width,canvas.height)
