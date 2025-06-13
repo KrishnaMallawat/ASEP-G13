df59cca24cf1379bd27796f114da54bd183432b0
Aayush Rahate <rahateaayush50@gmail.com>
2025-06-13 18:53:39 +0530

    Add files via upload

---
+++ GanitMitraDungeon/TOWN MAPS AND ASSETS/Town Map/OLD TOWN.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="OLD TOWN" tilewidth="16" tileheight="16" tilecount="5700" columns="95">
+ <image source="../../../Downloads/Town Map.png" width="1520" height="960"/>
+</tileset>
---
+++ GanitMitraDungeon/TOWN MAPS AND ASSETS/Town Map/town.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="town" tilewidth="16" tileheight="16" tilecount="5760" columns="96">
+ <image source="Town Map - Copy.png" width="1540" height="973"/>
+</tileset>
