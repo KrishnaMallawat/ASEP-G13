068ebf862b4395acf4b8cae148402dc92ea41a5e
Aayush Rahate <rahateaayush50@gmail.com>
2025-06-14 11:17:31 +0530

    Add files via upload

---
+++ Aqibe/memoryGame/flashCards.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify ,render_template , Blueprint, session, request
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from extension import db
+import random
+import os
+
+flashCards = Blueprint (
+    "flashCards", __name__,
+    template_folder="templates",
+    static_folder="static"
+)
+
+@flashCards.route("/")
+@login_required
+def loading():
+    return render_template('flashCardsloading.html')
+
+@flashCards.route("/home")
+@login_required
+def home():
+    return render_template('flashCardshome.html')
+
+@flashCards.route("/play")
+@login_required
+def play():
+    return render_template('flashCards.html')
+
+@flashCards.route("/how-to-play")
+@login_required
+def instructions():
+    return render_template('flashCardsInstructions.html')
